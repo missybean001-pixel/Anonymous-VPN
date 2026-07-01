9c55d5156a0bbf913450c0097d64764f9cb1a39c
missybean001-pixel <missybean001@gmail.com>
2026-07-01 00:17:12 -0400

    Revert MainActivity - remove weather dashboard fragment

--- app/src/main/java/com/missybean/vpn/MainActivity.kt
+++ app/src/main/java/com/missybean/vpn/MainActivity.kt
@@ -2,18 +2,10 @@ package com.missybean.vpn
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.missybean.vpn.weather.WeatherDashboardFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        
-        // Load Weather Dashboard Fragment
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.fragment_container, WeatherDashboardFragment())
-                .commit()
-        }
     }
 }
